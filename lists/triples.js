4a6e4c7c206c1605f955a94099fc83d434e9c2ee
David Robbins <robbins.david@gmail.com>
2011-09-12 09:17:19 -0500

    Full export support for HTML, N3, and RDF, including database name as default namespace.

--- lists/triples.js
+++ lists/triples.js
@@ -5,6 +5,11 @@ function(head, req) {
    * https://github.com/agrueneberg/Sessel
    */
 
+  //Setup the namespace
+  var default_namespace = req.query.default_namespace || "http://db/"+req.path[0]+"#",
+      ns = default_namespace,
+      default_namespace_abbreviation = req.query.default_namespace_abbreviation || req.path[0],
+      nsa = default_namespace_abbreviation;
 
   /**
    * Extract subject, predicate, object_type and object from key
@@ -29,12 +34,6 @@ function(head, req) {
    * @return boolean true if allowed, false otherwise
    */
   var permissionFilter = function(row, req) {
-      //var permission = row.value;
-      //if (permission == 'public' || (req.userCtx.name && permission == 'private')) {
-      //  return true;
-      //} else {
-      //  return false;
-      //}
       return true;
   };
 
@@ -49,30 +48,28 @@ function(head, req) {
         description += ' ';
         description += '&lt;' + triple[1] + '&gt;';
         description += ' ';
-        description += '&lt;' + triple[2] + '&gt;';
+        description += '"' + triple[2] + '"';
         description += ' .<br />'
         send(description);
       }
     }
   });
-/*
+
   // N3 output
   registerType('n3', 'text/n3');
   provides('n3', function() {
+    //setup the prefix
+    send('@prefix ' + nsa + ': <' + ns + '>\n');
     var row;
     while (row = getRow()) {
       if (permissionFilter(row, req)) {
         var triple = extractTriple(row, req);
         var description = '';
-        description += '<' + triple[0] + '>';
+        description += '<' + nsa + ':' + triple[0] + '>';
         description += ' ';
-        description += '<' + triple[1] + '>';
+        description += '<' + nsa + ':' + triple[1] + '>';
         description += ' ';
-        if (triple[2] === 'URI') {
-          description += '<' + triple[3] + '>';
-        } else if (triple[2] === 'Literal') {
-          description += '"' + triple[3] + '"';
-        }
+        description += '"' + triple[2] + '"';
         description += ' .\n';
         send(description);
       }
@@ -86,36 +83,21 @@ function(head, req) {
     var namespaceLookup = [];
     send('<?xml version="1.0" encoding="UTF-8"?>\n');
     send('<rdf:RDF xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#">');
+    send('<rdf:RDF xmlns:' + nsa + '="' + ns + '">');
     var row;
     while (row = getRow()) {
       if (permissionFilter(row, req)) {
         var triple = extractTriple(row, req);
-        // Extract namespace by only using the part before the last slash or hash
-        var matcher = triple[1].match(namespaceRegex);
-        var namespace = matcher[1];
-        var namespaceIndex = namespaceLookup.indexOf(namespace);
-        if (namespaceIndex === -1) {
-          namespaceLookup.push(namespace);
-          namespaceIndex = namespaceLookup.length - 1;
-        }
-        var identifier = matcher[2];
         // Generate description
         var description = '';
         description += '<rdf:Description rdf:about="' + triple[0] + '">';
-        if (triple[2] === 'URI') {
-          description += '<ns' + namespaceIndex + ':' + identifier + ' xmlns:ns' + namespaceIndex + '="' + namespace + '" rdf:resource="' + triple[3] + '" />';
-        } else if (triple[2] === 'Literal') {
-          description += '<ns' + namespaceIndex + ':' + identifier + ' xmlns:ns' + namespaceIndex + '="' + namespace + '">';
-          // Escape literals
-          description += escape(triple[3]);
-          description += '</ns' + namespaceIndex + ':' + identifier + '>';
-        }
+        description += '<'+nsa+':'+triple[1]+'>'+escape(triple[2])+'</'+nsa+':'+triple[1]+'>';
         description += '</rdf:Description>';
         send(description);
       }
     }
     send('</rdf:RDF>');
   });
-*/
+
 
 }
